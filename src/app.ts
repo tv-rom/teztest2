59fafa898d61376893517bffc0cc938dfb903bbd
tv-rom <46725470+tv-rom@users.noreply.github.com>
2021-12-07 01:00:29 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,65 @@ export class App {
       this.getBalance($("#address-input").val())
     );
   }
+  
+  
+const query = `
+  query collectorGallery($address: String!) {
+    hic_et_nunc_token_holder(where: {holder_id: {_eq: $address}, quantity: {_gt: "0"}, token: {supply: {_gt: "0"}}}, order_by: {id: desc}) {
+      token {
+        id
+        artifact_uri
+        display_uri
+        thumbnail_uri
+        timestamp
+        mime
+        title
+        description
+        supply
+        token_tags {
+          tag {
+            tag
+          }
+        }
+        creator {
+          address
+        }
+        swaps(where: {status: {_eq: "0"}}, order_by: {price: asc}) {
+          amount
+          amount_left
+          creator_id
+          price
+        }
+      }
+    }
+  }
+`;
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  const result = await fetch(
+    "https://api.hicdex.com/v1/graphql",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName
+      })
+    }
+  );
+
+  return await result.json();
+}
+
+async function doFetch() {
+  const { errors, data } = await fetchGraphQL(query, "collectorGallery", {"address":""});
+  if (errors) {
+    console.error(errors);
+  }
+  const result = data.hic_et_nunc_token_holder
+  console.log({ result })
+  return result
+}
 
   private showError(message: string) {
     $("#balance-output").removeClass().addClass("hide");
